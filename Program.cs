34e1a165d483aba94b5f2936870c119b43248944
Grymnir <magnus.carl.ekman@gmail.com>
2021-12-29 14:34:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tribonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with Tribonacci!");
+            Console.WriteLine("How many numbers do you want?");
+            int tribo = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 0;
+            int c = 1;
+
+            int d = a + b + c;
+            Console.WriteLine("Tribonacci series: ");
+            Console.Write(a + "\t" + b + "\t" + c);
+
+            for (int i = 4; i <= tribo; i++)
+            {
+                Console.Write("\t" + d);
+                a = b;
+                b = c;
+                c = d;
+                d = a + b + c;
+            }
+        }
+    }
+}
